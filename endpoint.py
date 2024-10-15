f3254d508414850d83eeaf4d8406fdbfcac3a35e
Marcos Danilo S. <dansouza91@outlook.com>
2024-10-15 16:32:59 -0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -3,9 +3,11 @@ from algoliasearch.search_client import SearchClient
 
 app = Flask(__name__)
 
-client = SearchClient.create("SIZMAGZTIC", "4bb7bc325b929f19973cc2436a860f96")
+# client = SearchClient.create("SIZMAGZTIC", "4bb7bc325b929f19973cc2436a860f96")
+# index = client.init_index('products_index')
 
-index = client.init_index('products_index')
+client = SearchClient.create("0A7VAU2G2V", "311bee779c8826859bb705b29ab2dab4")
+index = client.init_index('algolia')
 
 @app.route("/search/<name>", methods=["GET"])
 def obter_dados(name: str):
