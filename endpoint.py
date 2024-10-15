b1231eb62219f07162388d5aa62ee4dcba7d2d55
Marcos Danilo S. <dansouza91@outlook.com>
2024-10-15 15:41:33 -0300

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI
+from flask import Flask, jsonify, request
 from algoliasearch.search_client import SearchClient
 
-app = FastAPI()
+app = Flask(__name__)
 
 client = SearchClient.create("SIZMAGZTIC", "4bb7bc325b929f19973cc2436a860f96")
 
 index = client.init_index('products_index')
 
-@app.get("/search/{name}")
-def get_products(name: str):
+@app.route("/search/<name>", methods=["GET"])
+def obter_dados(name: str):
+    # Fazer a pesquisa no índice com o nome fornecido
     results = index.search(name)
+    
+    # Extrair os produtos dos resultados
+    products = results.get("hits", [])
 
-    products = results["hits"]
+    # Retornar os produtos como JSON
+    return jsonify(products)
 
-    return products
+# Iniciar o servidor
+if __name__ == '__main__':
+    app.run(debug=True)
